c563710ded4fdd5159733c924bc6dee3bc69c3fa
Etienne Dayre <etiennedayre10@gmail.com>
2021-11-25 18:27:57 +0100

    feat: add audio player

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -26,6 +26,25 @@ export default function Index() {
           You can find the source code for this project on the{" "}
           <a href="https://github.com/EtienneFR/audio-player-remix-app" target="_blank">Github project</a>
         </p>
+        <p>
+          Inspired by <a href="https://github.com/davidkpiano/frontend-masters-xstate-v2" target="_blank">David Kourshey's XState V2 course.</a>
+        </p>
+
+        <div className="player">
+          <div className="song">
+            <div className="title"><em>Song Title</em></div>
+            <div className="artist"><em>Artist</em></div>
+            <input type="range" className="scrubber" min="0" max="0" />
+            <output className="elapsed"></output>
+          </div>
+          <div className="controls">
+            <button className="play"></button>
+            <button className="pause"></button>
+            <button className="loading"></button>
+            <button className="skip"></button>
+            <button className="volume"></button>
+          </div>
+        </div>
       </main>
     </div>
   );
